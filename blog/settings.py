5ed1fad8ba42054db8d926d9c3c37449e713403e
yaggul <krommul@gmail.com>
2019-12-08 23:08:43 +0200

    Make the system work with sqlite3 database

--- blog/settings.py
+++ blog/settings.py
@@ -76,14 +76,21 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'blog',
+#         'USER': 'mitko',
+#         'PASSWORD': 'testvai',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'blog',
-        'USER': 'mitko',
-        'PASSWORD': 'testvai',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
